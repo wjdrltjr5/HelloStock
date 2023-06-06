38f0b56230e273200337be9c2d1959ea36849946
정기석(Gi Seok Jung) <48271492+wjdrltjr5@users.noreply.github.com>
2023-06-06 22:42:34 +0900

    feat : redis (#13) (#14)

--- src/main/java/deu/hellostock/HellostockApplication.java
+++ src/main/java/deu/hellostock/HellostockApplication.java
@@ -2,11 +2,13 @@ package deu.hellostock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication
 public class HellostockApplication {
 
